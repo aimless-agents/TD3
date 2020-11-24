c03c238114a4846f8d8836fbbc22cb5e5e5664d2
claire <basketballclaire@gmail.com>
2020-11-23 21:27:51 -0500

    save standard devs

--- main.py
+++ main.py
@@ -10,26 +10,27 @@ import OurDDPG
 import DDPG
 
 
-# Runs policy for X episodes and returns average reward
+# Runs policy for X episodes and returns reward average and std
 # A fixed seed is used for the eval environment
 def eval_policy(policy, env_name, seed, eval_episodes=10):
 	eval_env = gym.make(env_name)
 	eval_env.seed(seed + 100)
 
-	avg_reward = 0.
-	for _ in range(eval_episodes):
+	rewards = np.zeros(eval_episodes)
+	for i in range(eval_episodes):
 		state, done = eval_env.reset(), False
 		while not done:
 			action = policy.select_action(np.array(state))
 			state, reward, done, _ = eval_env.step(action)
-			avg_reward += reward
+			rewards[i] = reward
 
-	avg_reward /= eval_episodes
+	avg_reward = np.mean(rewards)
+	std_reward = np.std(rewards)
 
 	print("---------------------------------------")
 	print(f"Evaluation over {eval_episodes} episodes: {avg_reward:.3f}")
 	print("---------------------------------------")
-	return avg_reward
+	return [avg_reward, std_reward]
 
 
 if __name__ == "__main__":
