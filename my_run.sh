a9903fad94af603678fef7198323540f9b2755f5
tyu91 <tiffanyy9968@gmail.com>
2020-11-28 16:04:43 -0500

    updated run scripts

--- my_run.sh
+++ my_run.sh
@@ -7,6 +7,8 @@ do
 	python3.6 main.py \
 	--policy "TD3" \
 	--env "Hopper-v3" \
+    --discount 0.999 \
+    --tau 0.024 \
     --save_model \
 	--seed $i
 done
--- tune_run.sh
+++ tune_run.sh
@@ -4,8 +4,14 @@
 
 for ((i=0;i<1;i+=1))
 do 
-	python3.6 tune_td3.py \
+	# python3.6 tune_td3.py \
+	# --policy "TD3" \
+	# --env "Hopper-v3" \
+	# --seed $i
+
+    python3.6 tune_td3.py \
 	--policy "TD3" \
-	--env "Hopper-v3" \
+	--env "HalfCheetah-v3" \
+    --save_model \
 	--seed $i
 done
