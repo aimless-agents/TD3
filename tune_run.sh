b43dd7e2308a8db644fd608f3cddd6524c985c7a
tyu91 <tiffanyy9968@gmail.com>
2020-12-27 20:03:23 -0500

    minor tune_run update

--- tune_run.sh
+++ tune_run.sh
@@ -4,10 +4,9 @@
 
 for ((i=0;i<1;i+=1))
 do 
-	python3.6 tune_td3.py \
+	python distributed_tune.py \
 	--policy "TD3" \
 	--env "HalfCheetahMuJoCoEnv-v0" \
-    --smoke_test \
 	--seed $i
 
     # python3.6 tune_td3.py \
