ca16b38c72aa8af2c423290fcee174e0d956f5c1
tyu91 <tiffanyy9968@gmail.com>
2021-01-31 13:47:46 -0500

    elec cost refactor

--- our_reacher_env.py
+++ our_reacher_env.py
@@ -23,39 +23,33 @@ class OurReacherEnv(ReacherBulletEnv):
         self.robot.apply_action(a)
         self.scene.global_step()
 
-        state = self.robot.our_calc_state(self.g)  # sets self.to_target_vec
+        next_state = self.robot.our_calc_state(self.g)  # sets self.to_target_vec
 
-        # euclidean distance < epsilon
-        within_goal = np.sum(self.robot.to_target_vec ** 2) < self.epsilon
+        rewards = self.goal_cond_reward(a, next_state, self.g)
+        
+        self.HUD(next_state, a, False)
+        return next_state, rewards, False, {}
 
-        potential_old = self.potential
-        self.potential = self.robot.calc_potential()
+    # binary goal-conditioned reward function
+    # models reward from the transition state' (implicit) -> state
+    def goal_cond_reward(self, prev_action, next_state, goal):
+        assert goal.shape[0] == 2
+
+        to_goal_vec = np.array(next_state[0] + next_state[2], next_state[1] + next_state[3]) - goal
+        within_goal = np.sum(to_goal_vec ** 2) < self.epsilon
 
         electricity_cost = (
-                -0.10 * (np.abs(a[0] * self.robot.theta_dot) + np.abs(a[1] * self.robot.gamma_dot))  # work torque*angular_velocity
-                - 0.01 * (np.abs(a[0]) + np.abs(a[1]))  # stall torque require some energy
+                -0.10 * (np.abs(prev_action[0] * next_state[-3]) + np.abs(prev_action[1] * next_state[-1]))  # work torque*angular_velocity
+                - 0.01 * (np.abs(prev_action[0]) + np.abs(prev_action[1]))  # stall torque require some energy
         )
         electricity_exceeded = electricity_cost > 0.01
         stuck_joint_cost = -0.1 if np.abs(np.abs(self.robot.gamma) - 1) < 0.01 else 0.0
 
-        # self.rewards = [0.1 if within_goal and stuck_joint_cost == 0 else 0]
-        self.rewards = [0 if within_goal and stuck_joint_cost == 0 and not electricity_exceeded else -1]
+        potential_old = self.potential
+        self.potential = self.robot.calc_potential()
         self.original_rewards = sum([float(self.potential - potential_old), float(electricity_cost), float(stuck_joint_cost)])
-        self.HUD(state, a, False)
-        return state, sum(self.rewards), False, {}
-
-    # binary goal-conditioned reward function
-    # models reward from the transition state' (implicit) -> state
-    def goal_cond_reward(self, state, goal):
-        assert goal.shape[0] == 2
-
-        to_goal_vec = np.array(state[0] + state[2], state[1] + state[3]) - goal
-        within_goal = np.sum(to_goal_vec ** 2) < self.epsilon
-
-
-        stuck_joint_cost = -0.1 if np.abs(np.abs(self.robot.gamma) - 1) < 0.01 else 0.0
         # maybe make this 1???
-        return 0 if within_goal and stuck_joint_cost == 0 else -1
+        return 0 if within_goal and stuck_joint_cost == 0 and not electricity_exceeded else -5
 
     # pass in sigma=0 to get the original target goal state
     def sample_goal_state(self, sigma=1e-3):
--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ import numpy as np
 import torch
 from numba import jit
 import matplotlib.pyplot as plt
-from ray import tune
 
 
 class ReplayBuffer(object):
@@ -42,18 +41,6 @@ class ReplayBuffer(object):
         )
     
     def add_hindsight(self, trajectory, goal, env, k=4, fetch_reach=False):
-        # p_hindsight = 1.0 - (1.0 / (1.0 + k))
-        # hindsight_er = np.random.uniform(size=(len(trajectory) - 1)) < p_hindsight
-        # for i in range(len(trajectory) - 1):
-        #     state, action, next_state, gc_reward, done_bool = trajectory[i]
-        #     if hindsight_er[i]:
-        #         idx = np.random.choice(np.arange(i + 1, len(trajectory)))
-        #         future_state, _, _, _, _ = trajectory[idx]
-        #         x, next_x, gc_reward = self.updated_hindsight_experience(state, next_state, future_state, env, fetch_reach)
-        #     else:
-        #         x = np.concatenate([np.array(state["observation"]) if fetch_reach else np.array(state), goal])
-        #         next_x = np.concatenate([np.array(next_state["observation"]) if fetch_reach else np.array(next_state), goal])
-        #     self.add(x, action, next_x, gc_reward, done_bool)
         for _ in range(k): # factor to increase buffer by
             for i in range(len(trajectory)):
                 old_state, old_action, old_next_state, reward, old_done_bool = trajectory[i]
@@ -69,7 +56,7 @@ class ReplayBuffer(object):
                         new_goal = np.array([ng[0] + ng[2], ng[1] + ng[3]])
                         x = np.concatenate([old_state, new_goal])
                         next_x = np.concatenate([old_next_state, new_goal])
-                        gc_reward = env.goal_cond_reward(old_next_state, new_goal)
+                        gc_reward = env.goal_cond_reward(old_action, old_next_state, new_goal)
                     
                         self.add(x, old_action, next_x, gc_reward, old_done_bool)
 
@@ -77,6 +64,7 @@ class ReplayBuffer(object):
                 next_x = np.concatenate([np.array(old_next_state["observation"]) if fetch_reach else np.array(old_next_state), goal])
                 self.add(x, old_action, next_x, reward, old_done_bool)
 
+    # will need to refac if choose to use this again
     def updated_hindsight_experience(self, state, next_state, future_state, env, fetch_reach): 
         if fetch_reach:
             new_goal = future_state["desired_goal"]
@@ -208,6 +196,7 @@ def epsilon_calc(eps_upper, eps_lower, max_timesteps,
         return eps_upper * (1 - eps_lower) ** x
 
 def get_train_configuration(args):
+    from ray import tune
     config = {}
     if args.tune_run:
         if args.prioritized_replay:
